df3b6592eb10ac6121509a8622ff30f97440496a
unknown <sgy12303@gmail.com>
2021-02-25 01:26:38 +0900

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('hello potato!!!!!!!!!!!'));
+app.get('/', (req, res) => res.send('Bye potato.........'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
